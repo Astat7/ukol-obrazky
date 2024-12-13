4fcec5e5c2401d3652f50e66b513cc12f5c61d05
Radislav Střílek <96394461+Astat7@users.noreply.github.com>
2024-12-13 18:45:58 +0100

    Update pictures.py

--- pictures.py
+++ pictures.py
@@ -4,8 +4,7 @@ from PIL import ImageEnhance
 from PIL import ImageOps
 
 # načtení obrázku a uložení do proměnné
-image = Image.open("imageUkol\sailfin.jpg")
-
+image = Image.open("sailfin.jpg")
 
 # rozostření - vyší císlo -> větší rozostření
 blurred_1 = image.filter(ImageFilter.BoxBlur(10))
@@ -13,32 +12,32 @@ blurred_1 = image.filter(ImageFilter.BoxBlur(10))
 blurred_2 = image.filter(ImageFilter.BoxBlur((35, 1)))
 
 # uložení obrázků
-blurred_1.save("imageUkol\\blurred1.jpg")
-blurred_2.save("imageUkol\\blurred2.jpg")
+blurred_1.save("blurred1.jpg")
+blurred_2.save("blurred2.jpg")
 
 # změnení formátu
 png = image.convert("RGB")
-png.save("imageUkol\\converted.png")
+png.save("converted.png")
 
 # změna velikosti - zadává se hodnota v pixelech v tuple
 resized = image.resize((250, 250))
-resized.save("imageUkol\\resized.jpg")
+resized.save("resized.jpg")
 
 # oříznutí
 original_size = image.size
 cropped = image.crop((400, 0, original_size[0]/2, original_size[1]/2))
-cropped.save("imageUkol\\cropped.jpg")
+cropped.save("cropped.jpg")
 
 # kontrast - na vstupu je float od 0 do 1
 temp = ImageEnhance.Contrast(image)
 contrast = temp.enhance(0.25)
-contrast.save("imageUkol\\contrast.jpg")
+contrast.save("contrast.jpg")
 
 # světlost
 temp = ImageEnhance.Brightness(image)
 bright = temp.enhance(1.5)
-bright.save("imageUkol\\bright.jpg")
+bright.save("bright.jpg")
 
 # monochrome
 monochrome = ImageOps.grayscale(image)
-monochrome.save("imageUkol\\monochrome.jpg")
+monochrome.save("monochrome.jpg")
